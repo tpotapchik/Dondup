9b305f4abc3804b3bb918c720687aee219ff65cc
Labetskaya, Tatyana <t.labetskaya@itransition.com>
2017-05-15 16:03:43 +0300

    some refactor, js

--- js/main.js
+++ js/main.js
@@ -1,49 +1,66 @@
 $(function () {
-    $('input[type="text"], input[type="email"], input[type="tel"], textarea').focus(function () {
-        if ($(this).val() == $(this).attr("title")) {
-            $(this).val("");
-        }
-    }).blur(function () {
-        if ($(this).val() == "") {
-            $(this).val($(this).attr("title"));
-        }
-    });
-
     $('.component-select').chosen({
         disable_search: true,
         inherit_select_classes: true,
         width: "35px"
     });
 
-	$('#subslider1,#subslider2,#subslider3,#subslider4').slick({
-		slidesToShow: 1,
-		slidesToScroll: 1,
-		dots: false,
-		fade: true
-	});
+    $('#subslider1,#subslider2,#subslider3,#subslider4').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: false,
+        fade: true
+    });
 
-	$('#mainSlider').slick({
-		slidesToShow: 1,
-		slidesToScroll: 1,
-		dots: true,
-		fade: true
-	});
+    $('#mainSlider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true,
+        fade: true
+    });
 
-    if (Math.max(document.documentElement.clientWidth, window.innerWidth) <= 1024) {
-        $('#featuredSliderMobile').slick({
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            dots: true,
-            arrows: false,
-            fade: true
-        });
+    $('.additional-block-item .close-icon').click(function () {
+        var $this = $(this);
+        $this.closest('.header-top').slideUp(400);
+    });
 
+    $('.arrow-block').click(function () {
+
+        $('html, body').animate({
+            scrollTop: $(".scroll-top-js").offset().top
+        }, 2000);
+    });
+
+    function initSlider() {
+        if (Math.max(document.documentElement.clientWidth, window.innerWidth) <= 1024) {
+            $('#featuredSliderMobile').slick({
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                dots: true,
+                arrows: false,
+                fade: true
+            });
+        }
+    }
+    initSlider();
+
+    function initToggleItemsFootet() {
         $('.footer-item-wrap .footer-item-title').click(function () {
             var $this = $(this);
             $this.closest('.footer-item-wrap').toggleClass('opened');
         });
     }
 
+
+    if (Math.max(document.documentElement.clientWidth, window.innerWidth) <= 1024) {
+        initToggleItemsFootet();
+    }
+
+    $(window).resize(function () {
+        $('#featuredSliderMobile').unslick();
+        initSlider();
+        initToggleItemsFootet();
+    });
 
 });
 
